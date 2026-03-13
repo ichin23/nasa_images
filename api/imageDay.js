4fdf4741ec71c0a0d1c4fc07e45a4b2690e309c7
Pedro Manoel <pedroferreiramanoel@gmail.com>
2026-03-13 08:28:32 -0300

    fix api

--- api/imageDay.js
+++ api/imageDay.js
@@ -5,7 +5,7 @@ export default async function handler(request, response){
     const params = new URLSearchParams({
         "api_key": api_key
     })
-    const data = await fetch(`https://api.nasa.gov/planetary/apod?${params}`).then(async (response)=>await response.json())
+    const data = await fetch(`https://api.nasa.gov/planetary/apod?${params}`).then((response)=>response.json())
 
     return response.status(data.status).json(data)
 }
